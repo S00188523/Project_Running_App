594244f7d96c21d98fd53aa0fdbab2808132f14f
Adam Grace <S00188523@atu.ie>
2023-11-09 13:21:43 +0000

    Initial commit

---
+++ app/src/main/java/com/example/project_running_app/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.project_running_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView stepsTextView, timerTextView;
+    private Button startButton, stopButton, resetButton, showRunButton;
+    private int stepsCount = 0, secondsCount = 0;
+    private boolean isRunning = false;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        stepsTextView = findViewById(R.id.stepsTextView);
+        timerTextView = findViewById(R.id.timerTextView);
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+        resetButton = findViewById(R.id.resetButton);
+        showRunButton = findViewById(R.id.showRunButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRun();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopRun();
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetRun();
+            }
+        });
+
+        showRunButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isRunning) {
+                    showRunDetails();
+                }
+            }
+        });
+    }
+
+    private void startRun() {
+        isRunning = true;
+        handler.postDelayed(timerRunnable, 1000);
+    }
+
+    private void stopRun() {
+        isRunning = false;
+        handler.removeCallbacks(timerRunnable);
+        showRunButton.setEnabled(true);
+    }
+
+    private void resetRun() {
+        stepsCount = 0;
+        secondsCount = 0;
+        updateDisplay();
+    }
+
+    private void showRunDetails() {
+        Intent intent = new Intent(MainActivity.this, RunDetailsActivity.class);
+        intent.putExtra("stepsCount", stepsCount);
+        intent.putExtra("secondsCount", secondsCount);
+        startActivity(intent);
+    }
+
+    private Runnable timerRunnable = new Runnable() {
+        private static final int stepsPerSecond = 3;
+        @Override
+        public void run() {
+            secondsCount++;
+            stepsCount+= stepsPerSecond; // Increment steps
+            updateDisplay();
+            handler.postDelayed(this, 1000 / stepsPerSecond);
+
+            Log.d("MainActivity", "Seconds: " + secondsCount);
+        }
+    };
+
+    private void updateDisplay() {
+        stepsTextView.setText("Steps: " + stepsCount);
+        timerTextView.setText("Time: " + secondsCount + " seconds");
+    }
+}
---
+++ app/src/main/java/com/example/project_running_app/RunDetailsActivity.java
@@ -0,0 +1,56 @@
+package com.example.project_running_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class RunDetailsActivity extends AppCompatActivity {
+
+    private TextView dateTextView, metersTextView, caloriesTextView, timeTextView;
+    private Button returnButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run_details);
+
+        dateTextView = findViewById(R.id.dateTextView);
+        metersTextView = findViewById(R.id.metersTextView);
+        caloriesTextView = findViewById(R.id.caloriesTextView);
+        timeTextView = findViewById(R.id.timeTextView);
+        returnButton = findViewById(R.id.returnButton);
+
+        //  data from the previous activity
+        int stepsCount = getIntent().getIntExtra("stepsCount", 0);
+        long secondsCount = getIntent().getLongExtra("secondsCount", 0);
+
+        // Calculate run details
+        double meters = stepsCount * 0.8;
+        double calories = stepsCount * 0.04;
+
+        // Display run details
+        dateTextView.setText("Date: " + getCurrentDate());
+        metersTextView.setText("Meters: " + String.format(Locale.getDefault(), "%.2f", meters));
+        caloriesTextView.setText("Calories: " + String.format(Locale.getDefault(), "%.2f", calories));
+        timeTextView.setText("Time: " + secondsCount + " seconds");
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Close the current activity and return to the previous one
+            }
+        });
+    }
+
+    private String getCurrentDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        return dateFormat.format(new Date());
+    }
+}
